7300b63b3e5a7e405f4c06a36a3a0bf4b084ed69
CircularCatGit <colburnarlo27@gmail.com>
2026-08-29 21:15:59 -0500

    Create main.swift

---
+++ main.swift
@@ -0,0 +1,4 @@
+import UIKit
+
+// Explicitly register the global UIKit application entry lifecycle
+UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(ViewController.self))
