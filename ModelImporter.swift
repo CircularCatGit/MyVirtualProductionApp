2b635a586232657c3a1adaf15419c842c5f0d313
CircularCatGit <colburnarlo27@gmail.com>
2026-08-29 21:52:17 -0500

    Update ModelImporter.swift

--- ModelImporter.swift
+++ ModelImporter.swift
@@ -3,11 +3,11 @@ import UIKit
 import SceneKit
 import UniformTypeIdentifiers
 
-class ModelImporter: NSObject, UIDocumentPickerDelegate {
+public class ModelImporter: NSObject, UIDocumentPickerDelegate {
 
-    var delegate: ViewController?
+    public var delegate: ViewController?
 
-    func importModel() {
+    public func importModel() {
         let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.item, .data, .content])
         documentPicker.delegate = self
         documentPicker.allowsMultipleSelection = false
@@ -15,7 +15,7 @@ class ModelImporter: NSObject, UIDocumentPickerDelegate {
         delegate?.present(documentPicker, animated: true, completion: nil)
     }
 
-    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
         guard let url = urls.first else { return }
         
         if url.startAccessingSecurityScopedResource() {
@@ -27,19 +27,14 @@ class ModelImporter: NSObject, UIDocumentPickerDelegate {
                 DispatchQueue.main.async {
                     let importedNode = SCNNode()
                     
-                    // Pull all inner child elements together into one root node block container
                     for child in sceneKitScene.rootNode.childNodes {
                         importedNode.addChildNode(child)
                     }
                     
-                    // Normalize model scaling (Some models compile incredibly massive or tiny out of Blender)
                     importedNode.scale = SCNVector3(1.0, 1.0, 1.0)
-                    
-                    // Save into standby memory tracking slot
                     self.delegate?.pendingModelNode = importedNode
                     
-                    // Instruct the user with an on-screen alert banner
-                    let alert = UIAlertController(title: "Model Loaded", message: "Tap anywhere on a detected green surface mesh grid to anchor the 3D object down!", preferredStyle: .alert)
+                    let alert = UIAlertController(title: "Model Loaded", message: "Tap anywhere on a detected surface grid layout to anchor the 3D object down!", preferredStyle: .alert)
                     alert.addAction(UIAlertAction(title: "Got it", style: .default))
                     self.delegate?.present(alert, animated: true)
                 }
@@ -49,7 +44,7 @@ class ModelImporter: NSObject, UIDocumentPickerDelegate {
         }
     }
 
-    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+    public func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
         delegate?.dismiss(animated: true, completion: nil)
     }
 }
